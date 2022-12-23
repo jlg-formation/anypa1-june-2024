e3a8cfa8ddd2afcb86ae3fca20eff69f66aa298c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-12-23 11:29:38 +0100

    dev: added icons

--- front/src/app/stock/list/list.component.ts
+++ front/src/app/stock/list/list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faPlus,
+  faRotateRight,
+  faTrashAlt,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  faPlus = faPlus;
+  faTrashAlt = faTrashAlt;
+  faRotateRight = faRotateRight;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

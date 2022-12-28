2ef7864fd25abec1f59fddac4d3ec075cd98e97e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-12-28 13:27:14 +0100

    dev: added icon registry

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'front';
+  constructor(private matIconRegistry: MatIconRegistry) {
+    matIconRegistry.registerFontClassAlias(
+      'outlined',
+      'material-icons-outlined mat-ligature-font'
+    );
+  }
 }

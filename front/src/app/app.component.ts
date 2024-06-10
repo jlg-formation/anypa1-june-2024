1cb791b4713a0cee2b24aacd0b081554a8385449
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-06-10 15:29:59 +0200

    dev: migrated to angular 18

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LayoutModule } from './layout/layout.module';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, LayoutModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'front';
--- front/src/app/routes/legal/legal.component.ts
+++ front/src/app/routes/legal/legal.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-legal',
   templateUrl: './legal.component.html',
-  styleUrls: ['./legal.component.scss']
+  styleUrls: ['./legal.component.scss'],
+  standalone: true,
+  imports: [],
 })
 export class LegalComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

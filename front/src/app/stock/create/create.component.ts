77a497420e96ca110caa0b8ebd5d1fd78bf03ad3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-06-11 11:12:54 +0200

    dev: error management in template

--- front/src/app/stock/create/create.component.ts
+++ front/src/app/stock/create/create.component.ts
@@ -1,28 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCircleNotch, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { lastValueFrom, timer } from 'rxjs';
 import { ArticleService } from '../services/article.service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.scss'],
   standalone: true,
-  imports: [ReactiveFormsModule, FontAwesomeModule],
+  imports: [ReactiveFormsModule, FontAwesomeModule, JsonPipe],
 })
 export default class CreateComponent implements OnInit {
   errorMsg = '';
   f = this.fb.nonNullable.group({
-    name: ['Truc', [Validators.required]],
+    name: ['', [Validators.required, Validators.maxLength(10)]],
     price: [0, [Validators.required]],
     qty: [1, [Validators.required]],
   });

ed02f3c77904ac1118939b119781cf8dd277b262
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-06-11 16:03:29 +0200

    dev: observable

--- examples/observables/01-observable.js
+++ examples/observables/01-observable.js
@@ -4,13 +4,19 @@ const obs = new Observable((subscriber) => {
   subscriber.next("titi");
   subscriber.next(123);
 
-  setTimeout(() => {
+  const timer = setTimeout(() => {
+    console.log("run setTimeout");
     subscriber.next(false);
-    subscriber.complete();
+    subscriber.error(new Error("oups"));
   }, 1000);
+
+  return () => {
+    console.log("fin de vie");
+    clearTimeout(timer);
+  };
 });
 
-obs.subscribe({
+const subscription = obs.subscribe({
   next: (data) => {
     console.log("data: ", data);
   },
@@ -21,3 +27,7 @@ obs.subscribe({
     console.log("complete");
   },
 });
+
+// setTimeout(() => {
+//   subscription.unsubscribe();
+// }, 500);

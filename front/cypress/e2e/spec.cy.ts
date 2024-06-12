a5df2511c1537f88d2b59fa876d5a31aa84d92af
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-06-12 17:29:12 +0200

    dev: cypress

--- front/cypress/e2e/spec.cy.ts
+++ front/cypress/e2e/spec.cy.ts
@@ -1,6 +1,18 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running')
-  })
-})
+    cy.visit('/');
+    cy.contains('Gestion Stock');
+    cy.contains('Mentions Légales');
+    cy.contains('Voir le stock').click();
+    cy.get('a[title="Ajouter"]').click();
+
+    cy.get('input').eq(0).clear().type('aaa');
+    cy.get('input').eq(1).clear().type('12');
+    cy.get('input').eq(2).clear().type('15');
+
+    cy.contains('button', 'Ajouter').click();
+
+    cy.contains('td', 'aaa').click();
+    cy.get('button[title="Supprimer"]').click();
+  });
+});
